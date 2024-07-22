65156208572f822dc71675fb1f3417b832d09941
Kshitiz Rana <ranakshitizgm@gmail.com>
2024-07-22 11:13:09 +0530

    Update database_connect.py

--- src/database_connect.py
+++ src/database_connect.py
@@ -1,28 +1,32 @@
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 import mysql.connector
 
+load_dotenv()
 #Connection to database
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password = 'password',
+    password = os.getenv("PASSWORD"),
 )
 
 #Create a Cursor
 cr = db.cursor(buffered=True)
 
 #Create Database Inventory
+
 # cr.execute("DROP DATABASE INVENTORY;")
 # cr.execute("CREATE DATABASE INVENTORY;")
 cr.execute("SHOW DATABASES;")
 
 #Creating table Customers with name and membership
+
 cr.execute("USE INVENTORY;")
 # cr.execute("CREATE TABLE customers (name VARCHAR(255), membership CHAR(50));")
 cr.execute("SHOW TABLES;")
 
 #Insert Values in table Customers
+
 val = [
   ('Peter', 'basic'),
   ('Amy', 'premium'),
@@ -45,3 +49,9 @@ cr.executemany(query,val)
 db.commit()
 
 print(cr.rowcount,"was inserted.")
+
+# To print/show table table
+
+cr.execute("SELECT*FROM customers;")
+result = cr.fetchall()
+print(result)
